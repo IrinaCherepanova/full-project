e494e6f47a44784c45e459dab475a4949f492bbf
IrinaCherepanova <cherepanovaira@gmail.com>
2023-02-20 21:50:10 +0300

    adding pages to the project

---
+++ src/test/java/successTests/SuccessTests.java
@@ -0,0 +1,11 @@
+package successTests;
+
+import org.testng.annotations.Test;
+
+public class SuccessTests {
+    @Test
+    public void testStandardUser(){
+        System.out.println("Hello from tests");
+    }
+
+}
