801217c91921832081301f26287f9f3fc88434fd
IrinaCherepanova <cherepanovaira@gmail.com>
2023-02-23 19:18:32 +0300

    Update SuccessTests.java

--- src/test/java/tests/successTests/SuccessTests.java
+++ src/test/java/tests/successTests/SuccessTests.java
@@ -1,8 +1,5 @@
 package tests.successTests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -13,24 +10,24 @@ import tests.BaseTest;
 public class SuccessTests extends BaseTest {
 
 
-    protected WebDriver driver;
-    @Test(dataProvider = "getData", description = "Group of successful login test")
-    public void testSuccessLogin(String username, String password){
+    @Test(dataProvider = "getData", description = "Group of successful login tests")
+    public void testSuccessLogin(String username, String password) {
+
         driver.get("https://www.saucedemo.com/");
         LoginPage loginPage = new LoginPage(driver);
         ProductsPage productsPage = new ProductsPage(driver);
         loginPage.login(username, password);
         Assert.assertEquals(productsPage.getPageTitle(), "PRODUCTS");
         Assert.assertEquals(productsPage.getPageUrl(), "https://www.saucedemo.com/inventory.html");
+
     }
 
     @DataProvider
-    public Object[][] getData(){
+    public Object[][] getData() {
         return new Object[][]{
                 {"standard_user", "secret_sauce"},
                 {"problem_user", "secret_sauce"},
                 {"performance_glitch_user", "secret_sauce"}
         };
     }
-
 }
